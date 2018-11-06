21ef6bc3bf6616a549847816fa8f83e45bb0a08a
Farah Gallal <FGallal@integrant.com>
2018-11-06 17:33:08 +0200

    added unit optional attribute to the number field

--- src/Components/inputs/number/number-field.jsx
+++ src/Components/inputs/number/number-field.jsx
@@ -1,14 +1,63 @@
-import React from "react";
-import { ControlLabel, FormGroup, FormControl } from "react-bootstrap";
-const NumberField = props => {
-  return (
-    <FormGroup bsSize="large" id={props.field.key}>
-      <h4>
-        <ControlLabel>{props.field.label}</ControlLabel>
-      </h4>
-      <FormControl type="number" required={props.field.required} min="1" />
-    </FormGroup>
-  );
-};
+import React, { Component } from "react";
+import {
+  ControlLabel,
+  FormGroup,
+  FormControl,
+  InputGroup
+} from "react-bootstrap";
+
+class NumberField extends Component {
+  state = {
+    min: "",
+    max: "",
+    unit: ""
+  };
+
+  componentDidMount() {
+    if (this.props.field.min) {
+      this.setState({ min: this.props.field.min });
+    }
+    if (this.props.field.max) {
+      this.setState({ max: this.props.field.max });
+    }
+  }
+
+  addUnitAttribute() {
+    if (this.props.field.unit) {
+      return (
+        <InputGroup>
+          <InputGroup.Addon>{this.props.field.unit}</InputGroup.Addon>
+          <FormControl
+            type="number"
+            required={this.props.field.required}
+            min={this.state.min}
+            max={this.state.max}
+            placeholder={this.props.field.placeholder}
+          />
+        </InputGroup>
+      );
+    }
+    return (
+      <FormControl
+        type="number"
+        required={this.props.field.required}
+        min={this.state.min}
+        max={this.state.max}
+        placeholder={this.props.field.placeholder}
+      />
+    );
+  }
+  render() {
+    const { field } = this.props;
+    return (
+      <FormGroup bsSize="large" id={field.key}>
+        <h4>
+          <ControlLabel>{field.label}</ControlLabel>
+        </h4>
+        {this.addUnitAttribute()}
+      </FormGroup>
+    );
+  }
+}
 
 export default NumberField;
